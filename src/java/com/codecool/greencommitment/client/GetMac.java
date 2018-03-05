94beb6b9f3d5dfe31f3221d31e9469b058101b37
Laszlo Poroszkai <laszlo.poroszkai@gmail.com>
2018-03-05 13:40:13 +0100

    GetMac fixed

--- src/java/com/codecool/greencommitment/client/GetMac.java
+++ src/java/com/codecool/greencommitment/client/GetMac.java
@@ -1,9 +1,8 @@
 package com.codecool.greencommitment.client;
 
-import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
-import java.net.UnknownHostException;
+import java.util.Enumeration;
 
 public class GetMac {
 
@@ -11,28 +10,32 @@ public class GetMac {
 
         String macAddr = "";
 
-        InetAddress ip;
         try {
 
-            ip = InetAddress.getLocalHost();
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            while(networkInterfaces.hasMoreElements())
+            {
+                NetworkInterface network = networkInterfaces.nextElement();
+                byte[] mac = network.getHardwareAddress();
+                if(mac == null)
+                {
+                    System.out.println("null mac");
+                }
+                else
+                {
 
-            NetworkInterface network = NetworkInterface.getByInetAddress(ip);
-
-            byte[] mac = network.getHardwareAddress();
-
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < mac.length; i++) {
-                sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < mac.length; i++)
+                    {
+                        sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+                    }
+                    macAddr = sb.toString();
+                }
             }
+        } catch (SocketException e){
 
-            macAddr = sb.toString();
-
-
-        } catch (UnknownHostException e) {
             e.printStackTrace();
 
-        } catch (SocketException e) {
-            e.printStackTrace();
         }
 
         return macAddr;
