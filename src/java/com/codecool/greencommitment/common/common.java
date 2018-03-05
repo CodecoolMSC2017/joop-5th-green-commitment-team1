1b3647d9162c9bbb19194d9093edf8dd6ee17262
nagyrobert1990 <nagy.robert1990@gmailcom>
2018-03-05 11:14:52 +0100

    client and server classes modified

--- src/java/com/codecool/greencommitment/common/common.java
+++ src/java/com/codecool/greencommitment/common/common.java
@@ -1,8 +1,25 @@
 package com.codecool.greencommitment.common;
 
+import com.codecool.greencommitment.client.Client;
+import com.codecool.greencommitment.server.Server;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class common {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
 
+        Scanner scan = new Scanner(System.in);
+        String scanString = scan.nextLine();
+        scan.close();
+
+        if (scanString.equals("server")){
+            Server s = new Server();
+            s.runServer();
+        } else {
+            Client c = new Client();
+            c.runClient();
+        }
     }
 }
--- src/java/com/codecool/greencommitment/server/server.java
+++
@@ -1,4 +0,0 @@
-package com.codecool.greencommitment.server;
-
-public class server {
-}
