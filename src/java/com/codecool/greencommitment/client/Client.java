fc4349cbdaf7f7765652f4828bfc2fe70e682912
nagyrobert1990 <nagy.robert1990@gmailcom>
2018-03-05 11:17:18 +0100

    modified Client class to be runnable from common class

--- src/java/com/codecool/greencommitment/client/Client.java
+++ src/java/com/codecool/greencommitment/client/Client.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 // Client class
 public class Client
 {
-    public static void main(String[] args) throws IOException
+    public static void runClient() throws IOException
     {
         try
         {
