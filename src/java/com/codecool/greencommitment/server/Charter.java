2b7896fe8069313d435cb34f7fad66a11b763777
nagyrobert1990 <nagy.robert1990@gmailcom>
2018-03-06 13:37:49 +0100

    Charter class is ready to go. there is a temp.xml file to try it with.

--- src/java/com/codecool/greencommitment/server/Charter.java
+++ src/java/com/codecool/greencommitment/server/Charter.java
@@ -3,13 +3,16 @@ package com.codecool.greencommitment.server;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.block.BlockBorder;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
@@ -19,14 +22,24 @@ import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
 public class Charter extends JFrame {
 
-    public Charter() {
+    public Charter() throws ParserConfigurationException, SAXException, IOException {
 
         initUI();
     }
 
-    private void initUI() {
+    public void initUI() throws IOException, SAXException, ParserConfigurationException {
 
         XYDataset dataset = createDataset();
         JFreeChart chart = createChart(dataset);
@@ -41,30 +54,55 @@ public class Charter extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    private XYDataset createDataset() {
+    public XYDataset createDataset() throws ParserConfigurationException, IOException, SAXException {
 
         XYSeries series = new XYSeries("random");
-        /*  fill the charts points here. x - time; y - measurement points
-        series.add(18, 567);
-        series.add(20, 612);
-        series.add(25, 800);
+        List<Integer> times = new ArrayList<>();
+        List<Integer> values = new ArrayList<>();
+
+        File fXmlFile = new File("temp.xml");
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(fXmlFile);
+        doc.getDocumentElement().normalize();
+        NodeList nList = doc.getElementsByTagName("measurement");
+
+        int sizeof = nList.getLength();
+
+        for (int temp = 0; temp < nList.getLength(); temp++) {
+            Node nNode = nList.item(temp);
+            Node nTime = nNode.getChildNodes().item(1);
+            Node nValue = nNode.getChildNodes().item(3);
+            Node nType = nNode.getChildNodes().item(5);
+            long measTime = Long.parseLong(nTime.getTextContent());
+            int measValue = Integer.parseInt(nValue.getTextContent());
+            String measType = nType.getTextContent();
+
+            series.add(measTime, measValue);
+
+        }
+
+        XYSeriesCollection dataSet = new XYSeriesCollection();
+        /*
+        series.add(18, 33);
+        series.add(20, 28);
+        series.add(25, 22);
         series.add(30, 980);
         series.add(40, 1410);
         series.add(50, 2350);
         */
-        XYSeriesCollection dataset = new XYSeriesCollection();
-        dataset.addSeries(series);
+        dataSet.addSeries(series);
+        return dataSet;
 
-        return dataset;
     }
 
-    private JFreeChart createChart(XYDataset dataset) {
+    public JFreeChart createChart(XYDataset dataSet) {
 
         JFreeChart chart = ChartFactory.createXYLineChart(
-                "Average salary per age",
-                "Age",
-                "Salary (€)",
-                dataset,
+                "Team1 Random Measurement",
+                "Time",
+                "Temperature (C)",
+                dataSet,
                 PlotOrientation.VERTICAL,
                 true,
                 true,
@@ -75,7 +113,7 @@ public class Charter extends JFrame {
 
         XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
         renderer.setSeriesPaint(0, Color.RED);
-        renderer.setSeriesStroke(0, new BasicStroke(2.0f));
+        renderer.setSeriesStroke(0, new BasicStroke(1.5f));
 
         plot.setRenderer(renderer);
         plot.setBackgroundPaint(Color.white);
@@ -88,7 +126,7 @@ public class Charter extends JFrame {
 
         chart.getLegend().setFrame(BlockBorder.NONE);
 
-        chart.setTitle(new TextTitle("Average Salary per Age",
+        chart.setTitle(new TextTitle("Team1 Random Measurement",
                         new Font("Serif", java.awt.Font.BOLD, 18)
                 )
         );
@@ -97,11 +135,10 @@ public class Charter extends JFrame {
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
 
-        SwingUtilities.invokeLater(() -> {
-            Charter ex = new Charter();
-            ex.setVisible(true);
-        });
+        Charter ex = new Charter();
+        ex.setVisible(true);
+
     }
 }
