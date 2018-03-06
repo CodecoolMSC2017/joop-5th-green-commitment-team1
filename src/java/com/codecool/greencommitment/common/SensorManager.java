8065e31a0e733dca976c3e9a668575d8d968f93e
Tamas Kiss <sauriaster@gmail.com>
2018-03-06 09:18:06 +0100

    added sensor meneger, might not used later

---
+++ src/java/com/codecool/greencommitment/common/SensorManager.java
@@ -0,0 +1,43 @@
+package com.codecool.greencommitment.common;
+
+import com.profesorfalken.jsensors.JSensors;
+import com.profesorfalken.jsensors.model.components.Components;
+import com.profesorfalken.jsensors.model.components.Cpu;
+import com.profesorfalken.jsensors.model.sensors.Fan;
+import com.profesorfalken.jsensors.model.sensors.Temperature;
+
+import java.util.List;
+
+public class SensorManager {
+    private Components components;
+
+    public SensorManager() {
+        this.components = JSensors.get.components();
+        detectAll();
+    }
+
+    public void detectAll(){
+        List<Cpu> cpus = components.cpus;
+        if (cpus != null){
+            for (final Cpu cpu : cpus) {
+                System.out.println("Found CPU component: " + cpu.name);
+                if (cpu.sensors != null) {
+                    System.out.println("Sensors: ");
+
+                    //Print temperatures
+                    List<Temperature> temps = cpu.sensors.temperatures;
+                    for (final Temperature temp : temps) {
+                        System.out.println(temp.name + ": " + temp.value + " C");
+                    }
+
+                    //Print fan speed
+                    List<Fan> fans = cpu.sensors.fans;
+                    for (final Fan fan : fans) {
+                        System.out.println(fan.name + ": " + fan.value + " RPM");
+                    }
+                }
+            }
+        }
+    }
+
+}
